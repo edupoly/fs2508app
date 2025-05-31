53ed26904f151f6a5d0edef6beec0f22df37d2c6
edupoly <info.edupoly@gmail.com>
2025-05-31 12:01:24 +0530

    sdfsdf

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,9 @@ function App() {
   return (
     <div className='p-5 border border-success'>
       <h1>Welcome to ReactJS App Development base practice</h1>
-      <Counter></Counter>
+      <h2>Vishnu</h2>
+      <Counter i={10} iv={5} dv={13}></Counter>
+      <Counter i={30} iv={20} dv={15}></Counter>
     </div>
   )
 }
--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,14 +1,18 @@
 import React from "react"
-function Counter(){
-    var [counter,setCounter] = React.useState(0)
+function Counter(props){
+    console.log(props);
+    var [counter,setCounter] = React.useState(props.i)
     function inc(){
-        setCounter(counter+1)
+        setCounter(counter+props.iv)
+    }
+    function dec(){
+        setCounter(counter+props.dv)
     }
     return(
         <div className="border border-danger p-3 m-3">
             <h1>Counter: {counter} </h1>
             <button onClick={()=>{inc()}}>Inc</button>
-            <button>Dec</button>
+            <button onClick={()=>{dec()}}>Dec</button>
         </div>
     )
 }
