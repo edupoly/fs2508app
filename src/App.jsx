ed4d3d48f759bf26e5dc764322e7b704013f5a88
edupoly <info.edupoly@gmail.com>
2025-06-03 13:34:16 +0530

    sdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 
 import './App.css'
-
+import Counter from './Counter'
 function App() {
 
 
   return (
     <div className='p-5 border border-success'>
       <h1>Welcome to ReactJS App Development base practice</h1>
-      
+      <Counter></Counter>
     </div>
   )
 }
