0d1c816300843b8481006904595f38def949595c
edupoly <info.edupoly@gmail.com>
2025-06-04 12:46:26 +0530

    nnna

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 
 import './App.css'
+import Child from './Child'
+import Parent from './Parent'
 
 function App() {
 
 
   return (
-    <div className='p-5 border border-success'>
+    <div className='container p-5 border border-success'>
       <h1>Welcome to ReactJS App Development base practice</h1>
-      
+      <Parent></Parent>
     </div>
   )
 }
