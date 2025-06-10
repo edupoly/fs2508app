385b2af8e41006b01e6cc99894c93baad2c18481
edupoly <info.edupoly@gmail.com>
2025-06-10 12:41:07 +0530

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 
+import { Outlet,Link } from 'react-router-dom'
+import Aboutus from './Aboutus'
 import './App.css'
+import Contactus from './Contactus'
+import Home from './Home'
 
 function App() {
 
 
   return (
-    <div className='p-5 border border-success'>
+    <div className='p-2 m-2 border border-success'>
       <h1>Welcome to ReactJS App Development base practice</h1>
-      
+      <Link to="/home">Home</Link>
+      &nbsp; &nbsp;
+      <Link to="/aboutus">Aboutus</Link>
+      &nbsp; &nbsp;
+      <Link to="/contactus">Contactus</Link>
+      &nbsp; &nbsp;
+      <Link to="/products">Products</Link>
+      <Outlet></Outlet>
     </div>
   )
 }
