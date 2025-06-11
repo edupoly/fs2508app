b08c998c8e16fff836e89ca1135a07a519b50681
edupoly <info.edupoly@gmail.com>
2025-06-11 13:29:35 +0530

    reduxadvanced

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { decAction, incAction } from './store/actioncreators';
 function Counter(props) {
-    console.log("Counter Component Rendered");
+    console.log("Counter Component Rendered",props);
   return (
     <div className="border border-5 border-success p-3 m-3">
-        <h1>Counter:{props.counterReducer.count}</h1>
-        <button onClick={()=>{props.dispatch({type:"INC"})}}>Increment</button>
-        <button onClick={()=>{props.dispatch({type:"DEC"})}}>Decrement</button>
+        <h1>Counter:{props.count}</h1>
+        <button onClick={()=>{props.increment()}}>Increment</button>
+        <button onClick={()=>{props.decrement()}}>Decrement</button>
     </div>
   )
 }
 
-export default connect((store)=>{return store})(Counter)
+function mapStateToProps(state){return state.counterReducer}
+function mapDispatchToProps(dispatch){
+  return {
+    increment:()=>{dispatch(incAction())},
+    decrement:()=>{dispatch(decAction())}
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Counter)
---
+++ src/store/actioncreators.js
@@ -0,0 +1,9 @@
+export function incAction(){
+    return {type:"INC"}
+}
+export function decAction(){
+    return {type:"DEC"}
+}
+export function addTodoAction(ntd){
+    return {type:"ADDTODO",payload:ntd}
+}
