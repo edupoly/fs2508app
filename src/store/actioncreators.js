6f7ad20c1d98a40bfe495fa83124150c67ac54fb
edupoly <info.edupoly@gmail.com>
2025-06-12 12:15:37 +0530

    mappingstateanddispatch

--- src/store/actioncreators.js
+++ src/store/actioncreators.js
@@ -1,9 +1,11 @@
+import { INC,DEC,ADDTODO } from "./actions"
+
 export function incAction(){
-    return {type:"INC"}
+    return {type:INC}
 }
 export function decAction(){
-    return {type:"DEC"}
+    return {type:DEC}
 }
 export function addTodoAction(ntd){
-    return {type:"ADDTODO",payload:ntd}
+    return {type:ADDTODO,payload:ntd}
 }
