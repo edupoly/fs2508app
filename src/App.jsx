1886ba0f6f0d4756e3752786ff76c14a7ae98829
edupoly <info.edupoly@gmail.com>
2025-06-13 12:26:48 +0530

    store

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 
 import './App.css'
+import Counter from './features/counter/Counter'
+import Todolist from './features/todolist/Todolist'
 
 function App() {
 
 
   return (
-    <div>
+    <div className='border border-5 border-danger p-2 m-2'>
       <h1>Welcome to ReactJS App Development</h1>
+      <Todolist></Todolist>
+      <Counter></Counter>
     </div>
   )
 }
